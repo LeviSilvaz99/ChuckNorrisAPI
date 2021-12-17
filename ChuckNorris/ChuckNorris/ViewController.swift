736899c7dd837533c0334aa5b1953f5e600b0eca
Luis Ferreira <luistf.mobile@gmail.com>
2021-12-16 23:06:18 -0300

    Create project

---
+++ ChuckNorris/ChuckNorris/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ChuckNorris
+//
+//  Created by Luis Ferreira on 16/12/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
